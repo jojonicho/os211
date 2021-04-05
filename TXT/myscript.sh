0ea0470cbf303a3f2d0888b16d13e26e94cb7d36
jojonicho <jojonicho181@gmail.com>
2021-04-06 00:55:39 +0700

    update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Cicak bin Kadal
-# Tue 13 Oct 2020 10:37:14 AM WIB
-
-FILES="my*.txt my*.sh"
-SHA="SHA256SUM"#!/bin/bash
 # Copyright (C) 2020-2021 Cicak Bin Kadal
 # https://www.youtube.com/watch?v=KAXK07ni9gU
 
@@ -11,6 +6,7 @@ SHA="SHA256SUM"#!/bin/bash
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+# REV05 Sat 27 Mar 00:12:28 WIB 2021
 # REV04 Mon 15 Mar 19:27:52 WIB 2021
 # REV03 Sun 14 Mar 18:21:27 WIB 2021
 # REV02 Fri 12 Mar 13:40:58 WIB 2021
@@ -41,7 +37,7 @@ done
 popd
 
 rm -f $HOME/RESULT/fakeDODOL
-for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
+for II in $HOME/RESULT/myW*.tar.bz2.asc ; do
    echo "Check and move $II..."
    [ -f $II ] && mv -f $II .
 done
@@ -63,87 +59,4 @@ gpg --verify $SHA.asc $SHA
 
 exit 0
 
-
-
-
-echo "rm -f $SHA $SHA.asc"
-rm -f $SHA $SHA.asc
-
-echo "sha256sum $FILES > $SHA"
-sha256sum $FILES > $SHA
-
-echo "sha256sum -c $SHA"
-sha256sum -c $SHA
-
-echo "gpg -o $SHA.asc -a -sb $SHA"
-gpg -o $SHA.asc -a -sb $SHA
-
-echo "gpg --verify $SHA.asc $SHA"
-gpg --verify $SHA.asc $SH#!/bin/bash
-# Copyright (C) 2020-2021 Cicak Bin Kadal
-# https://www.youtube.com/watch?v=KAXK07ni9gU
-
-# This free document is distributed in the hope that it will be 
-# useful, but WITHOUT ANY WARRANTY; without even the implied 
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-# REV04 Mon 15 Mar 19:27:52 WIB 2021
-# REV03 Sun 14 Mar 18:21:27 WIB 2021
-# REV02 Fri 12 Mar 13:40:58 WIB 2021
-# REV01 Tue 13 Oct 10:37:14 WIB 2020
-# START Mon 28 Sep 21:05:04 WIB 2020
-
-# ATTN:
-# You new to set "REC2" with your own Public-Key Identity!
-# Check it out with "gpg --list-key"
-
-REC2="cbk@dummy"
-REC1="operatingsystems@vlsm.org"
-FILES="my*.asc my*.txt my*.sh"
-SHA="SHA256SUM"
-
-[ -d $HOME/RESULT ] || mkdir -p $HOME/RESULT
-pushd $HOME/RESULT
-for II in W?? ; do
-    [ -d $II ] || continue
-    TARFILE=my$II.tar.bz2
-    TARFASC=$TARFILE.asc
-    rm -f $TARFILE $TARFASC
-    echo "tar cfj $TARFILE $II/"
-    tar cfj $TARFILE $II/
-    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
-    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
-done
-popd
-
-rm -f $HOME/RESULT/fakeDODOL
-for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
-   echo "Check and move $II..."
-   [ -f $II ] && mv -f $II .
-done
-
-echo "rm -f $SHA $SHA.asc"
-rm -f $SHA $SHA.asc
-
-echo "sha256sum $FILES > $SHA"
-sha256sum $FILES > $SHA
-
-echo "sha256sum -c $SHA"
-sha256sum -c $SHA
-
-echo "gpg -o $SHA.asc -a -sb $SHA"
-gpg -o $SHA.asc -a -sb $SHA
-
-echo "gpg --verify $SHA.asc $SHA"
-gpg --verify $SHA.asc $SHA
-
-exit 0
-
-
-A
-
-exit 0
-
-# Mon Sep 28 21:05:04 WIB 2020
-# Tue 29 Sep 2020 11:02:39 AM WIB
 
